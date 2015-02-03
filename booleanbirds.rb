170eab87a08d1a79cdfdf437336afbe651422dda
mykia <mykiajls@gmail.com>
2015-02-03 10:27:10 -0600

    add new file to git

---
+++ booleanbirds.rb
@@ -0,0 +1,43 @@
+# puts "1! Birds on a wire. Ha, ha, ha!!!"
+
+# for n in 2...10
+#   puts"#{n}! Birds on a wire! Hazzzahh!"
+# end
+
+# 2..100 represents a RANGE in Ruby.
+# .. includes the final digit
+# ... excludes the final digit
+
+
+# x = 0
+
+# while x <= 10
+#   puts "#{x} is the loneliest number everrrrrrr!"
+#   # x = x + 1
+#   x+=1 # This means the same as the previous line
+
+# end
+# puts "
+
+# We have exited the loop!
+
+# "
+
+# 5.times do |x|
+#   puts "We are at number #{x + 1}!?!?!?"
+#   if x ==3
+#     puts "We are at numero tres!"
+#   end
+# end
+
+
+#The following two chunks of code do the same thing
+
+# for n in 1..5
+#   puts "We are at number #{n}."
+# end
+
+
+
+# 1.upto(5){ |n| puts "We are at number #{n}"}
+# puts"\n"
